451b36d8ac2abd1ab7e0ac638c1af7ed4b1b7b92
Aidan Ryan <ryan.aidan@gmail.com>
2012-08-26 22:57:56 -0700

    Initial commit

---
+++ ReCaptchaFake/Controllers/CaptchaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace ReCaptchaFake.Controllers
+{
+    public class CaptchaController : Controller
+    {
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Verify(string privatekey, string remoteip, string challenge, string response)
+        {
+            // default to web.config setting
+            string result = ConfigurationManager.AppSettings["CaptchaResult"];
+
+            // allow override in user-entered response
+            if (response.Equals("true", StringComparison.OrdinalIgnoreCase) || response.Equals("false", StringComparison.OrdinalIgnoreCase))
+                result = response;
+
+            return Content(result.ToLower() + "\n");
+        }
+    }
+}
---
+++ ReCaptchaFake/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ReCaptchaFake
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}",
+                new { controller = "Captcha", action = "Verify" });
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
